dddc6ef805ce76997b33e392f014069226565377
hiimstevejin <jingle5695@gmail.com>
2025-06-27 19:10:27 -0500

    Enhance timeline component with additional events and improve styling

--- src/components/TimelineClient.tsx
+++ src/components/TimelineClient.tsx
@@ -5,68 +5,99 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import { FaLandmark, FaChartLine, FaUsers, FaRocket } from "react-icons/fa";
+import {
+  FaLandmark,
+  FaChartLine,
+  FaMicrophone,
+  FaBriefcase,
+  FaUsers,
+  FaRocket,
+} from "react-icons/fa";
 
 export default function TimelineClient() {
   return (
     <VerticalTimeline>
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        contentStyle={{ background: "#155dfc", color: "#fff" }}
-        contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-        date="Summer 2024"
-        iconStyle={{ background: "#00AAEF", color: "#fff" }}
+        contentStyle={{ background: "#00AAEF", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  #00AAEF" }}
+        iconStyle={{ background: "#154DEC", color: "#fff" }}
         icon={<FaLandmark />}
       >
-        <h3 className="vertical-timeline-element-title">
-          KISO&apos;s First Event !
-        </h3>
+        <h3 className="text-xl font-bold">KISO&apos;s First Event</h3>
+        <h4 className="vertical-timeline-element-subtitle">Summer 2024</h4>
         <p>
-          Korean International Student Organization&apos;s first unofficial
-          event took place in Seoul, South Korea to welcome class of 2028
+          Korean International Student Organization&apos;s first unofficial event took place in Seoul, South Korea to welcome class of 2028.
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        contentStyle={{ background: "#008937", color: "#fff" }}
-        contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-        date="Fall 2024"
-        iconStyle={{ background: "#00AAEF", color: "#fff" }}
+        contentStyle={{ background: "#00AAEF", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  #00AAEF" }}
+        iconStyle={{ background: "#154DEC", color: "#fff" }}
         icon={<FaChartLine />}
       >
-        <h3 className="vertical-timeline-element-title">KISO as CIO</h3>
+        <h3 className="text-xl font-bold">KISO as CIO</h3>
+        <h4 className="vertical-timeline-element-subtitle">Fall 2024</h4>
         <p>
-          Kiso&apos; founding team officially registers KISO as a Contracted
+          KISO&apos;s founding team officially registers KISO as a Contracted
           Independent Organization of University of Virginia.
         </p>
-      </VerticalTimelineElement>{" "}
+      </VerticalTimelineElement>
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        contentStyle={{ background: "#8400DE", color: "#fff" }}
-        contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-        date="Spring 2024"
-        iconStyle={{ background: "#00AAEF", color: "#fff" }}
+        contentStyle={{ background: "#00AAEF", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  #00AAEF" }}
+        iconStyle={{ background: "#154DEC", color: "#fff" }}
+        icon={<FaMicrophone />}
+      >
+        <h3 className="text-xl font-bold">
+          First Official Membership Training
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle">Fall 2024</h4>
+        <p>
+          KISO hosts its first official membership training trip to strengthen
+          bonds between members of its community.
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: "#00AAEF", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  #00AAEF" }}
+        iconStyle={{ background: "#154DEC", color: "#fff" }}
+        icon={<FaBriefcase />}
+      >
+        <h3 className="text-xl font-bold">
+          First career fair funded by the Korean Embassy
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle">Fall 2024</h4>
+        <p>
+          KISO was funded $500 to participate in a career fair officialy hosted by the Korean Embassy in the United States to explore post graduate career opportunities. 
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: "#00AAEF", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  #00AAEF" }}
+        iconStyle={{ background: "#154DEC", color: "#fff" }}
         icon={<FaUsers />}
       >
-        <h3 className="vertical-timeline-element-title">
-          First Successful Semester !
-        </h3>
+        <h3 className="text-xl font-bold">First Successful Semester</h3>
+        <h4 className="vertical-timeline-element-subtitle">Spring 2024</h4>
         <p>
           KISO wraps up its first successful semester with over 100 members and
           10+ events hosted for the first semester
         </p>
-      </VerticalTimelineElement>{" "}
+      </VerticalTimelineElement>
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        contentStyle={{ background: "#CD3600", color: "#fff" }}
-        contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-        date="Spring 2024"
-        iconStyle={{ background: "#00AAEF", color: "#fff" }}
+        contentStyle={{ background: "#00AAEF", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  #00AAEF" }}
+        iconStyle={{ background: "#154DEC", color: "#fff" }}
         icon={<FaRocket />}
       >
-        <h3 className="vertical-timeline-element-title">
-          KISO Website Launch !
-        </h3>
+        <h3 className="text-xl font-bold">KISO Website Launch</h3>
+        <h4 className="vertical-timeline-element-subtitle">Summer 2025</h4>
         <p>
           KISO website officially launches to help members find resources
           related to joining KISO and attending University of Virginia as an
--- src/pages/timeline.tsx
+++ src/pages/timeline.tsx
@@ -1,12 +1,12 @@
 import dynamic from "next/dynamic";
 
-const TimelineClient = dynamic(() => import('@/components/TimelineClient'), {
+const TimelineClient = dynamic(() => import("@/components/TimelineClient"), {
   ssr: false,
 });
 
 export default function TimelinePage() {
   return (
-    <main className="min-h-screen py-12 px-4">
+    <main className="min-h-screen py-12 px-4 bg-[#E7EFFE]">
       <TimelineClient />
     </main>
   );
